10c0cd0b1bf430a39ead8aaaa81cef11f0be1eab
asiradnan <asiradnan23@gmail.com>
2024-12-04 12:29:32 +0600

    Effects clicks

--- .next/server/chunks/ssr/[root of the server]__44a334._.js
+++ .next/server/chunks/ssr/[root of the server]__44a334._.js
@@ -773,7 +773,10 @@ function Home() {
             }
         };
         fetchData();
-    }, []);
+    }, [
+        shortUrl,
+        copied
+    ]);
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
         className: "min-h-screen bg-gradient-to-br from-purple-50 via-indigo-50 to-purple-50",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
--- .next/static/chunks/app_50b791._.js
+++ .next/static/chunks/app_50b791._.js
@@ -750,7 +750,10 @@ function Home() {
             }["Home.useEffect.fetchData"];
             fetchData();
         }
-    }["Home.useEffect"], []);
+    }["Home.useEffect"], [
+        shortUrl,
+        copied
+    ]);
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
         className: "min-h-screen bg-gradient-to-br from-purple-50 via-indigo-50 to-purple-50",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
